fe52780bda8342e1decad2524e2bc19761476e24
kit-kosatka <dimon_barkov@icloud.com>
2026-05-24 16:03:11 +0300

    Add services

--- app/services/employee.py
+++ app/services/employee.py
@@ -1,5 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.services.department import DepartmentNotFound
+from app.repositories import department as department_repo
+from app.repositories import employee as employee_repo
+from app.schemas.employee import EmployeeCreate
 
 
 
-async def reassign(from_dept_id: int, to_dept_id: int, session: AsyncSession) -> None:
+async def create_employee(dept_id: int, employee: EmployeeCreate, session: AsyncSession):
+    department = await department_repo.get_by_id(dept_id=dept_id, session=session)
+    if not department:
+        raise DepartmentNotFound()
+    return await employee_repo.create(department_id=dept_id, employee=employee, session=session)
